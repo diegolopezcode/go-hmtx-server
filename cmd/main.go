df72db22029a01f20c841e056d9d2bc1162480eb
Diego Lopez <diegolopez.coder@gmail.com>
2023-09-27 13:19:04 -0500

    Add[HTMX]: Create server with gin
    What: Create server with gin
    Why: To start the project
    
    002-Add[HTMX]: Create server with gin.patch | (download)

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"github.com/diegolopezcode/go-hmtx-server/api/routes"
+	"github.com/gin-gonic/gin"
+)
+
 func main() {
-	//
+	app := gin.Default()
+
+	routes.SetupRoutes(app)
+	app.Run(":8080")
 }
